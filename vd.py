dafbb046a29a4b486ba4fee48fceeac3aaf8bd74
m87 <m87blackhole@gmail.com>
2016-01-23 14:59:30 +0100

    init

--- vd.py
+++ vd.py
@@ -1,7 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
 
 
 
+class Data(object):
 
+    """Docstring for Data. """
+
+    def __init__(self):
+        """TODO: to be defined1. """
+        
+
+
+class VocalDetector(object):
+
+    """Docstring for VocalDetector. """
+
+    def __init__(self):
+        """TODO: to be defined1. """
+        
 
 
 def main:
